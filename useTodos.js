d66f03e335292fe708d8af58cbcc84cf86d3a4ce
moises-herrera <moisesherrera037@gmail.com>
2023-06-25 14:11:56 -0500

    initial commit

---
+++ useTodos.js
@@ -0,0 +1,49 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer';
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    };
+    dispatchTodo(action);
+  };
+
+  const handleDeleteTodo = (todoId) => {
+    const action = {
+      type: '[TODO] Remove Todo',
+      payload: todoId,
+    };
+    dispatchTodo(action);
+  };
+
+  const handleToggleTodo = (todoId) => {
+    const action = {
+      type: '[TODO] Toggle Todo',
+      payload: todoId,
+    };
+    dispatchTodo(action);
+  };
+
+  const pendingTodosCount = todos.filter(({ done }) => !done).length;
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todosCount: todos.length,
+    pendingTodosCount,
+  };
+};
